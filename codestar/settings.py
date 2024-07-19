cac40a6045f6d03e7c8cd2f1026d9eaa4d1c2998
Steven Benton <steve@bentonemail.uk>
2024-07-19 09:41:33 +0000

    Doc Add Heroku deployment items.

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ne_451i*bn(+j(stknd8^ol%_xoymy9go01otty_g!xsn8hps'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-stevebenton6-djangoblog-okysaug7h58.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-stevebenton6-djangoblog-okysaug7h58.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
